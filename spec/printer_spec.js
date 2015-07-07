79e267d944f7746df054ac68dfdc0ba7d8bbc3c8
Antoine Lepee <alepee@gmail.com>
2015-07-07 16:46:38 +0200

    disable jasmine spec (need fake data from cups binaries)

--- spec/printer_spec.js
+++ spec/printer_spec.js
@@ -1,14 +1,16 @@
+// TODO: need false data from `lpq` and `lpstat` to create tests
+
 var Printer = require('../printer.js');
 
 describe('get list of installed printers', function() {
-  var list = Printer.list();
+  // var list = Printer.list();
 
-  it('should return a list of printers name', function() {
-    expect(list.length > 0).toBe(true);
-  });
+  // it('should return a list of printers name', function() {
+  //   expect(list.length > 0).toBe(true);
+  // });
 
-  it('should verify printer existance', function() {
-    expect(Printer.match(list[0])).toBe(true);
-    expect(Printer.match('fakePrinterName')).toBe(false);
-  });
+  // it('should verify printer existance', function() {
+  //   expect(Printer.match(list[0])).toBe(true);
+  //   expect(Printer.match('fakePrinterName')).toBe(false);
+  // });
 });
